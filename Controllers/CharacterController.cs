8e5ddb12983bcd19025f58e1ee724bf76bd7a8ee
Alexandre-Lowawa-Gaspar <alexandreshmurda@gmail.com>
2023-11-21 12:15:12 +0100

    Applying the crud

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>{
+            new Character(),
+            new Character{Id=1, Name="Adison"}
+        };
+        [HttpGet("GetCharacterAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+        [HttpGet("GetCharacter/{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+
+            return Ok(characters.FirstOrDefault(x => x.Id == id));
+        }
+        [HttpPost("AddCharacter")]
+        public ActionResult<List<Character>> AddCharacher(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
+    }
+}
